9b774618737efd7142734d7b983eed1a7a108d89
kabaww <hello@joshmutia.com>
2025-06-12 21:34:33 +0800

    test commit
    Co-authored-by: beetlejuice41 <beetlejuice41@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<div class="text-6xl bg-amber-950 hover:bg-amber-200">Hello World</div>
