b5982121110195333fe9b48ba78066b57e321f6e
beetlejuice41 <bea99castro@gmail.com>
2025-06-30 18:55:28 +0800

    case studies: removed the div parts from main page. transferred it to the components. main page: component portfolio page: component
    case studies main page: component

--- src/lib/components/CaseStudyPreview.svelte
+++ src/lib/components/CaseStudyPreview.svelte
@@ -1,4 +1,46 @@
-<section class="bg-gray-100 p-8 rounded-lg">
-  <h2 class="text-2xl font-bold mb-4">Case Study</h2>
-  <p>Here's a case study highlight... duplicate to ah</p>
-</section>
+<div>
+        <div class="flex flex-row justify-between max-w-7xl mx-auto p-6">
+          <h2 class="text-xl heading-font-weight">Case Studies</h2>
+          <a href="/casestudy" class="text-secondary-shade transition-colors flex flex-row">View All
+            <svg class="ml-2 mt-1" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 20 20"><path fill="currentColor" d="M7 1L5.6 2.5L13 10l-7.4 7.5L7 19l9-9z"/></svg>
+          </a>
+        </div> 
+        <!-- scrollable photos of featured work. when clicked, it will take to the portfolio page and open the modal -->
+        <div class="flex flex-row overflow-x-auto snap-x snap-mandatory scrollbar-hide md:overflow-visible max-w-7xl mx-auto">
+          <div class="flex gap-4 px-4 md:pb-12 pb-4 snap-x snap-mandatory md:grid md:grid-cols-3 md:gap-6 md:overflow-visible md:max-h-[50vh]">
+          <!-- card 1 -->
+            <div class="min-w-[80%] snap-start bg-white rounded-xl shadow-md overflow-hidden aspect-[4/5] md:aspect-auto flex flex-col">
+              <img src="https://r2.viankacastro.com/logos/thumbnail.png" alt="Project 1" class="w-full h-2/3 object-cover">
+              <div class="p-4 flex flex-col flex-1 justify-evenly">
+                <h3 class="font-semibold text-lg">Avante Guard Logo Rebranding</h3>
+                <p class="text-sm text-gray-600 line-clamp-2">Avante-Guard is a production company specializing in high-quality video content. Prior to consultation, the business already had an established identity, which I've only worked around it to accompany them into the revision of the logo.</p>
+                <!-- "View Project" button, need to revise! -->
+                <a href="/portfolio" class="text-sm font-medium text-secondary-shade hover:underline"> View Case Study →
+                </a>
+              </div>
+            </div>
+            <div class="min-w-[80%] snap-start bg-white rounded-xl shadow-md overflow-hidden aspect-[4/5] md:aspect-auto flex flex-col">
+              <img src="https://r2.viankacastro.com/logos/school%20of%20health%20sciences.png" alt="Project 2" class="w-full h-2/3 object-cover">
+              <div class="p-4 flex flex-col flex-1 justify-evenly">
+                <h3 class="font-semibold text-lg line-clamp-1">School of Health and Sciences Logo</h3>
+                <p class="text-sm text-gray-600 line-clamp-2 break-words">School of Health Sciences is a relatively new department in Mapúa University that is in collaboration with Arizona State University. It offers BS Biology, BS Medical Technology, AB Psychology and BS Psychology. More information here.</p>
+                <!-- "View Project" button, need to revise! -->
+                <a href="/portfolio" class="text-sm font-medium text-secondary-shade hover:underline"> View Case Study →
+                </a>
+              </div>
+            </div>
+            <div class="min-w-[80%] snap-start bg-white rounded-xl shadow-md overflow-hidden aspect-[4/5] md:aspect-auto flex flex-col">
+              <img src="https://r2.viankacastro.com/TopCat%20KV%20with%20Product%20-%20SQUARE%20-%20STATIC.jpg" alt="Project 3" class="w-full h-2/3 object-cover">
+              <div class="p-4 flex flex-col flex-1 justify-evenly">
+                <h3 class="font-semibold text-lg line-clamp-1">TopCat PH: Facebook Social Media Growth</h3>
+                <p class="text-sm text-gray-600 line-clamp-2">TopCat is the feline-focused sister brand of TopBreed, offering premium, science-backed nutrition tailored specifically for cats.</p>
+                <!-- "View Project" button, need to revise! -->
+                <a href="/portfolio" class="text-sm font-medium text-secondary-shade hover:underline"> View Case Study →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        
+       </div>
--- src/routes/casestudy/+page.svelte
+++ src/routes/casestudy/+page.svelte
@@ -1,4 +1,7 @@
-<section class="bg-gray-100 p-8 rounded-lg">
-  <h2 class="text-2xl font-bold mb-4">Case Study</h2>
-  <p>Here's a case study highlight... main root</p>
-</section>
+<script lang="ts">
+    import CaseStudyPreview from '$lib/components/CaseStudyPreview.svelte';
+</script>
+
+<div class="space-y-8">
+        <CaseStudyPreview/>
+        </div>
